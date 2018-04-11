445a23658a6c63b9743cc834eaf57f4938efca74
Chris Lewis <cl_myles@hotmail.com>
2018-04-11 21:40:17 +1000

    Add & roll multiple dice

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
-function rollDie(dieElementName) {
-    document.getElementById(dieElementName).innerText = Math.floor(Math.random() * 6 + 1);
+function addDie() {
+    var diceElement = document.getElementById('dice');
+    var die = document.createElement("div");
+    die.className = 'die d6';
+    die.innerText = 'd6';
+    diceElement.appendChild(die);
+}
+
+function rollDie(element) {
+    element.innerText = Math.floor(Math.random() * 6 + 1);
+}
+
+function rollDice() {
+    var dice = document.getElementsByClassName('d6');
+    for (var i = 0; i < dice.length; i++)
+    {
+        rollDie(dice[i]);
+        // dice[i].innerText = Math.floor(Math.random() * 6 + 1);
+    }
 }
