77cdf9a243dbd907e36415fb4bbf29690589e4a5
Chris Lewis <cl_myles@hotmail.com>
2018-04-28 19:55:50 +1000

    Refactored the dice rolling function using REGEX

--- scripts.js
+++ scripts.js
@@ -6,7 +6,8 @@ function addDie(type) {
     diceElement.appendChild(die);
 }
 
-function rollDie(element, range) {
+function rollDie(element) {
+    var range = parseInt(element.className.match(/d(\d+)/)[1]);
     var result = Math.floor(Math.random() * range + 1);
     element.innerText = result;
     return result;
@@ -14,36 +15,11 @@ function rollDie(element, range) {
 
 function rollDice() {
     var result = 0;
-    var d4 = document.getElementsByClassName('d4');
-    var d6 = document.getElementsByClassName('d6');
-    var d8 = document.getElementsByClassName('d8');
-    var d10 = document.getElementsByClassName('d10');
-    var d12 = document.getElementsByClassName('d12');
-    var d20 = document.getElementsByClassName('d20');
+    var dice = document.getElementsByClassName('die');
     
-    for (var i = 0; i < d4.length; i++)
+    for (var i = 0; i < dice.length; i++)
     {
-        result += rollDie(d4[i], 4);
-    }
-    for (var i = 0; i < d6.length; i++)
-    {
-        result += rollDie(d6[i], 6);
-    }
-    for (var i = 0; i < d8.length; i++)
-    {
-        result += rollDie(d8[i], 8);
-    }
-    for (var i = 0; i < d10.length; i++)
-    {
-        result += rollDie(d10[i], 10);
-    }
-    for (var i = 0; i < d12.length; i++)
-    {
-        result += rollDie(d12[i], 12);
-    }
-    for (var i = 0; i < d20.length; i++)
-    {
-        result += rollDie(d20[i], 20);
+        result += rollDie(dice[i]);
     }
 
     document.getElementById('result').innerText = 'Result: ' + result;
